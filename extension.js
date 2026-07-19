jkavery/all-windows

commit 5e6216abe84fa057689abd20037704753ed585bc

Remove redundant logging + add exception handling

extension.js
@@ -616,12 +616,14 @@ export default class AllWindowsExtension extends Extension {
     }
 
     async SaveSession() {
-        this.#log.log("Saving window states...")
-        await this.#allWindowsStates.saveWindowPositions("SaveSession triggered via DBus")
+        await this.#allWindowsStates.saveWindowPositions("DBus SaveSession").catch(e => {
+            this.#log.exception("SaveSession caught", e);
+        });
     }
 
     async RestoreSession() {
-        this.#log.log("Restoring window states...")
-        await this.#allWindowsStates.restoreWindowPositions("RestoreSession triggered via DBus", this.#log)
+        await this.#allWindowsStates.restoreWindowPositions("DBus RestoreSession").catch(e => {
+            this.#log.exception("RestoreSession caught", e);
+        });
     }
 }

commit a73bf155290760edebaae433ee7910e1ea175930

Fix no pass log bug introduced in 5e6216abe84f

extension.js
@@ -620,7 +620,7 @@ export default class AllWindowsExtension extends Extension {
     }
 
     async RestorePositions() {
-        await this.#allWindowsStates.restoreWindowPositions("DBus RestorePositions").catch(e => {
+        await this.#allWindowsStates.restoreWindowPositions("DBus RestorePositions", this.#log).catch(e => {
             this.#log.exception("RestorePositions caught", e);
         });
     }